f3a36f163482733af818c0a0989d2a956cb03a3a
Jake8078 <jake8078@hotmail.com>
2013-11-29 14:38:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testlandia::Application.config.secret_key_base = 'f1186732a97bad52eefa1a04eb4b485bcbe8c372ea4fe72a40bd42ecb23e8dc9002cf2e884db2e089a7737bbb5e487358bfa2d245634b3ef802590aa3291ac84'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Testlandia::Application.config.session_store :cookie_store, key: '_testlandia_session'
